444ccd8dbdab09c6cebf19334499092aeb5f3e6a
Vasiliy <Kiriluk_v_r@mail.ru>
2022-07-04 11:46:11 +0300

    добавил приветствие в начале приложения

--- src/main/java/ru/homework/tasktracker/TaskTrackerApplication.java
+++ src/main/java/ru/homework/tasktracker/TaskTrackerApplication.java
@@ -1,13 +1,20 @@
 package ru.homework.tasktracker;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskTrackerApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner addGreeting() {
+		return args -> System.out.println("Привет! Если не знаешь, что можно вводить, напиши help.");
 	}
 
 }
